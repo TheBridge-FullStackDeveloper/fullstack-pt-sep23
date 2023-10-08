97b8e39fd3c119f1c93b2e8c78c0558f9a8d4654
AranBeitia <arantza.beitia@gmail.com>
2023-10-08 18:16:10 +0200

    Feat: ui test exercise

---
+++ JavaScript/UI tests/Ex-SumArray/script.js
@@ -0,0 +1,22 @@
+/**
+ * Devuelve la suma de todos los número de un array recibido por argumentos, excluye de la suma el valor más bajo y el más alto
+ * Si recibimos null, undefined, array vacío o un array con un número o dos número, deberá devolverse 0
+ *
+ * OBLIGATORIO: USA TESTS!
+ *
+ * Hazlo de dos maneras distintas:
+ * 1- Como quieras
+ * 2- Sin el método de Array "sort" ni con "reduce"
+ *
+ * Datos para pruebas: input | ouput
+ * sumArray(null) | 0
+ * sumArray([ ]) | 0
+ * sumArray([ 3 ]) | 0
+ * sumArray([ 3, 5 ]) | 0
+ * sumArray([ 6, 2, 1, 8, 10 ]) | 16
+ * sumArray([ 0, 1, 6, 10, 10 ]) | 17
+ * sumArray([ -6, -20, -1, -10, -12 ]) | -28
+ * sumArray([ -6, 20, -1, 10, -12 ]) | 3
+ */
+
+function sumArray(array) {}
